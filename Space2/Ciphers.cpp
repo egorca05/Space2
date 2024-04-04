02bb55f43a48da11313c16818b8170465264b7b9
Egor <gapeevegor@gmail.com>
2024-04-04 20:34:32 +0300

    1

---
+++ Space2/Ciphers.cpp
@@ -0,0 +1,25 @@
+//#include "header.h"
+//#include "MyFunction.h"
+//
+//using namespace std;
+//
+//void MenuCiphers() {
+//    Uchar ch = 0;
+//    do {
+//        CLS;
+//        SetTextColor(CL_LIGHT_RED);
+//        cout << "Меню Лабиринта:\n";
+//        SetColor();
+//        cout << "1 - Лабиринт\n";
+//        cout << "ESC - Выход из программы\n";
+//        SetTextColor(CL_LIGHT_YELLOW);
+//        cout << "Выберите пункт меню\n";
+//        SetColor();
+//
+//        ch = _getch();
+//        switch (ch) {
+//        case '1': Labyrint(); break;
+//        case '2':  break;
+//        }
+//    } while (ch != 27);
+//}
